dab26fc57af3d6764040261ce96fd787b6c2f498
KelvinWinsters <kelvinwinsters@gmail.com>
2020-10-01 13:01:45 +0300

    initial commit

---
+++ 001/hello world.py
@@ -0,0 +1,11 @@
+from datetime import date
+
+
+def one():
+    print("Hello World")
+    user_name = input("Enter your name")
+    print(f"Welcome {user_name}")
+
+
+if __name__ == '__main__':
+    one()
